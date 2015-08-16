2e2dc79d35790e8b5a2ed4d2d28bc40f2ce1cb79
Bob_JIANG <jdw19930725@gmail.com>
2015-08-16 20:41:57 +0800

    The Java way of implementing LRU cache, I do love the C++ way of doing it and the "touch" method

---
+++ src/Problem7/Node.java
@@ -0,0 +1,13 @@
+package Problem7;
+
+public class Node {
+	public Node prev;
+	public Node next;
+	public String[] results;
+	public String query;
+
+	public Node(String q, String[] res) {
+		results = res;
+		query = q;
+	}
+}
