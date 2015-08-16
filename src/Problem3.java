0f1e77825910db6c2fb30df26f164b9b3b891c20
Bob_JIANG <jdw19930725@gmail.com>
2015-08-16 20:59:14 +0800

    For question one, just use the standard bitmap algorithm, use one bit to represent whether a number has appeared.  For question two, use blocks, try to determine which block contains the missing number first, then deal with that block

---
+++ src/Problem3.java
@@ -0,0 +1,41 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ * Created by Bob_JIANG on 8/16/2015.
+ */
+public class Problem3 {
+    public static long numberOfInts = ((long) Integer.MAX_VALUE) + 1;
+    public static byte[] bitfield = new byte [(int) (numberOfInts / 8)];
+
+    public static void findOpenNumber() throws FileNotFoundException {
+        Scanner in = new Scanner(new FileReader("Chapter 10/Question10_3/input_file_q10_3.txt"));
+        while (in.hasNextInt()) {
+            int n = in.nextInt ();
+			/* Finds the corresponding number in the bitfield by using
+			 * the OR operator to set the nth bit of a byte
+			 * (e.g., 10 would correspond to the 2nd bit of index 2 in
+			 * the byte array). */
+            bitfield [n / 8] |= 1 << (n % 8);
+        }
+
+        for (int i = 0; i < bitfield.length; i++) {
+            for (int j = 0; j < 8; j++) {
+				/* Retrieves the individual bits of each byte. When 0 bit
+				 * is found, finds the corresponding value. */
+                if ((bitfield[i] & (1 << j)) == 0) {
+                    System.out.println (i * 8 + j);
+                    return;
+                }
+            }
+        }
+    }
+
+    //For question one, just use the standard bitmap algorithm, use one bit to represent whether a number has appeared.
+    // For question two, use blocks, try to determine which block contains the missing number first, then deal with that block
+    public static void main(String[] args)  throws IOException {
+        findOpenNumber();
+    }
+}
